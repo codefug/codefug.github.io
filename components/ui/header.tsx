f2b5092c3a8df7d28909ddce54f656d46cf0903f
codefug <robot9917@naver.com>
2025-01-02 03:28:03 +0900

    feat: theme기능이 포함된 헤더 컴포넌트 구현

---
+++ components/ui/header.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { useTheme } from 'next-themes'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { NAVIGATION_ITEMS } from '@/constants/navigation'
+import { useEffect, useState } from 'react'
+import { cn } from '@/lib/utils'
+import Switch from './switch'
+
+export default function Header() {
+  const [isChecked, setIsChecked] = useState(false)
+  const { setTheme, theme } = useTheme()
+  const pathName = `/${usePathname().split('/')[1]}`
+  useEffect(() => {
+    if (theme === 'dark') setIsChecked(true)
+    else setIsChecked(false)
+  }, [theme])
+
+  return (
+    <header className="fixed left-0 right-0 top-0 flex items-center justify-between p-4 shadow-lg dark:shadow-white">
+      <Link
+        href="/"
+        passHref
+        className="flex items-center gap-2 hover:opacity-95 hover:drop-shadow-lg"
+      >
+        <Image
+          src="/images/main-logo.png"
+          alt="logo"
+          height={0}
+          width={0}
+          className="h-7 w-full"
+          sizes="28px"
+          priority
+        />
+        <div className="hidden whitespace-nowrap text-lg font-bold text-black dark:text-white md:block">
+          Codefug Blog
+        </div>
+      </Link>
+      <section className="flex items-center gap-4 text-sm font-semibold md:text-base">
+        <Link
+          href={NAVIGATION_ITEMS.portfolio.href}
+          passHref
+          className={cn(
+            `text-gray-400 hover:text-black hover:underline hover:drop-shadow-lg hover:dark:text-white`,
+            pathName === NAVIGATION_ITEMS.portfolio.href
+              ? 'text-black dark:text-white'
+              : ''
+          )}
+        >
+          {NAVIGATION_ITEMS.portfolio.label}
+        </Link>
+        <Link
+          href={NAVIGATION_ITEMS.categories.href}
+          passHref
+          className={cn(
+            `text-gray-400 hover:text-black hover:underline hover:drop-shadow-lg hover:dark:text-white`,
+            pathName === NAVIGATION_ITEMS.categories.href
+              ? 'text-black dark:text-white'
+              : ''
+          )}
+        >
+          {NAVIGATION_ITEMS.categories.label}
+        </Link>
+        <Link
+          href={NAVIGATION_ITEMS.about.href}
+          passHref
+          className={cn(
+            `text-gray-400 hover:text-black hover:underline hover:drop-shadow-lg hover:dark:text-white`,
+            pathName === NAVIGATION_ITEMS.about.href
+              ? 'text-black dark:text-white'
+              : ''
+          )}
+        >
+          {NAVIGATION_ITEMS.about.label}
+        </Link>
+        <Switch
+          checked={isChecked}
+          onCheckedChange={() =>
+            theme === 'light' ? setTheme('dark') : setTheme('light')
+          }
+        />
+      </section>
+    </header>
+  )
+}
---
+++ constants/navigation.ts
@@ -0,0 +1,18 @@
+export const NAVIGATION_ITEMS = {
+  home: {
+    label: 'Home',
+    href: '/',
+  },
+  portfolio: {
+    label: 'Portfolio',
+    href: '/portfolio',
+  },
+  categories: {
+    label: 'Categories',
+    href: '/categories',
+  },
+  about: {
+    label: 'About',
+    href: '/about',
+  },
+}
