e732310bd6e9f0ee8ec4badeb37b44e05adefae0
codefug <robot9917@naver.com>
2025-01-02 03:28:03 +0900

    feat: theme기능이 포함된 헤더 컴포넌트 구현

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import ThemeProvider from '@/app/provider/theme-provider'
+import Header from '@/components/ui/header'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Codefug Blog',
+  description: 'project experiences and development notes',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="min-h-screen">
+            <div className="mt-16">{children}</div>
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
