dc03591f9005fa4756a1b93032268a79f1a26ac8
codefug <robot9917@naver.com>
2025-01-03 23:23:06 +0900

    feat: 헤더 정리 및 사이드바 구현

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,6 +99,16 @@ export default {
           gray: "hsl(var(--quote-gray))",
           "sky-blue": "hsl(var(--quote-sky-blue))",
         },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
