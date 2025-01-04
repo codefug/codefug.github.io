79702f27b92088bf621300630dab75a38407be93
codefug <robot9917@naver.com>
2025-01-05 02:49:18 +0900

    refactor: navigation item 관련 데이터 로직 수정

--- components/sidebar/sidebar-button/index.tsx
+++ components/sidebar/sidebar-button/index.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { useSidebar } from "@/components/ui/sidebar";
-import { PanelLeftClose } from "lucide-react";
+import { ReactNode } from "react";
 
-export default function SidebarButton() {
+export default function SidebarButton({ children }: { children: ReactNode }) {
   const { toggleSidebar } = useSidebar();
   return (
     <button
       onClick={toggleSidebar}
-      className="flex h-7 w-7 items-center justify-center hover:bg-accent hover:text-black"
+      className="flex h-7 w-7 items-center justify-center rounded-md hover:bg-accent hover:text-black hover:dark:text-white"
     >
-      <PanelLeftClose size={20} />
+      {children}
     </button>
   );
 }
