61c93ec3fedfcebbccdf719a206d8406e12d0fe5
codefug <robot9917@gmail.com>
2025-04-13 05:26:00 +0900

    feat: about 페이지 구현

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,6 @@
 import type { MDXComponents } from "mdx/types";
-import Image, { ImageProps } from "next/image";
 import { createElement, ReactNode } from "react";
 import Callout from "./components/mdx/callout";
-import CustomLink from "./components/mdx/custom-link";
 import processCallout from "./util/callout";
 
 function getHeadingIndexWithCloser() {
@@ -71,21 +69,21 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       const { type, title, content } = processCallout(children);
       return <Callout type={type} title={title} content={content} />;
     },
-    a: CustomLink,
-    img: (props) => {
-      const { src } = props;
-      const url = src.startsWith("public") ? src.slice(6) : src;
-      return (
-        <Image
-          sizes="900px"
-          style={{ width: "50%", height: "auto" }}
-          width={0}
-          height={0}
-          {...(props as ImageProps)}
-          src={url}
-        />
-      );
-    },
+    // a: CustomLink,
+    // img: (props) => {
+    //   const { src } = props;
+    //   const url = src.startsWith("public") ? src.slice(6) : src;
+    //   return (
+    //     <Image
+    //       sizes="900px"
+    //       style={{ width: "50%", height: "auto" }}
+    //       width={0}
+    //       height={0}
+    //       {...(props as ImageProps)}
+    //       src={url}
+    //     />
+    //   );
+    // },
     ...components,
   };
 }
