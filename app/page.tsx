4d83bd9ff3dde304cfd587695034126c8a3ac293
codefug <robot9917@gmail.com>
2025-04-13 05:54:47 +0900

    feat: footer 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import PostGallery from "@/components/postGallery";
-import Header from "@/components/ui/header";
 import { getFrontMatterList } from "@/lib/posts";
 
 export default function Home() {
   const frontMatterList = getFrontMatterList();
 
   return (
-    <>
-      <Header />
-      <div>
-        <PostGallery postInfoList={frontMatterList} />
-      </div>
-    </>
+    <div>
+      <PostGallery postInfoList={frontMatterList} />
+    </div>
   );
 }
---
+++ components/ui/footer.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="mt-6 border-t bg-background py-6">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <div className="flex items-center justify-center gap-4">
+            {/* 소셜 링크 */}
+            <div className="flex space-x-4">
+              <Link
+                href="mailto:robot9917@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                <Image
+                  src="/icons/email.svg"
+                  alt="Email"
+                  width={24}
+                  height={24}
+                  className="transition-transform hover:scale-110"
+                />
+                <span className="sr-only">GitHub</span>
+              </Link>
+            </div>
+            <div className="flex space-x-4">
+              <Link
+                href="https://github.com/codefug"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-muted-foreground transition-colors hover:text-foreground"
+              >
+                <Image
+                  src="/icons/github.svg"
+                  alt="GitHub"
+                  width={24}
+                  height={24}
+                  className="transition-transform hover:scale-110"
+                />
+                <span className="sr-only">GitHub</span>
+              </Link>
+            </div>
+          </div>
+
+          {/* 저작권 정보 */}
+          <p className="text-sm text-muted-foreground">
+            © {new Date().getFullYear()} CodeFug Blog
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
