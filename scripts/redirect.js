ef6322176f29ca8467feefeee77f264a3bccacfa
codefug <robot9917@gmail.com>
2025-04-13 18:42:22 +0900

    Updates

--- scripts/redirect.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  if (typeof window !== "undefined") {
-    const { redirect } = sessionStorage;
-    delete sessionStorage.redirect;
-    if (redirect && redirect !== window.location.pathname)
-      window.history.replaceState(null, null, redirect);
-  }
-})();
