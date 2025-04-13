faede7009730c15c337d861d405e0a0c27d6c95f
codefug <robot9917@gmail.com>
2025-04-13 17:31:49 +0900

    Updates

--- script/redirect.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const { redirect } = sessionStorage;
-  delete sessionStorage.redirect;
-  if (redirect && redirect !== window.location.href)
-    window.history.replaceState(null, null, redirect);
-})();
