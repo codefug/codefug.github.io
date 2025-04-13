df02361f48fa4936f88d67df251751a35b610b01
codefug <86544979+codefug@users.noreply.github.com>
2025-04-14 05:44:57 +0900

    feat: 사이드 바 프로필 관련 기초 세팅

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -12,11 +12,14 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { NAVIGATION_ITEMS } from "@/constants/navigation";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { useRef } from "react";
+import { ScrollArea } from "./scroll-area";
 
 export default function AppSidebar() {
   const { toggleSidebar } = useSidebar();
@@ -24,54 +27,59 @@ export default function AppSidebar() {
 
   return (
     <Sidebar ref={ref}>
-      <SidebarContent className="dark:border-r dark:border-r-gray-500">
-        <SidebarGroup>
-          <SidebarGroupLabel className="mb-4 flex items-center justify-between pl-4 pr-0">
-            <Link
-              href="/"
-              passHref
-              className="flex items-center gap-2 hover:opacity-95 hover:drop-shadow-lg"
-            >
-              <Image
-                src="/images/main-logo.png"
-                alt="logo"
-                height={0}
-                width={0}
-                className="h-7 w-full"
-                sizes="28px"
-              />
-              <div className="whitespace-nowrap text-lg font-bold text-black dark:text-white">
+      <ScrollArea className="h-full w-full overflow-y-auto">
+        <SidebarContent className="group relative">
+          <Image
+            src="/images/sidebar-profile-background.jpg"
+            alt="Profile Background"
+            width={300}
+            height={150}
+            className="absolute h-[150px] w-full object-cover"
+          />
+          <SidebarGroup>
+            <SidebarGroupLabel className="flex justify-center px-4 text-sm font-bold text-white">
+              About Me
+            </SidebarGroupLabel>
+            <SidebarGroupContent className="flex min-h-full flex-col items-center px-2">
+              <Avatar className="h-52 w-52">
+                <AvatarImage src="/images/me.jpg" />
+                <AvatarFallback className="group-hover:text-black group-hover:dark:text-white">
+                  Codefug
+                </AvatarFallback>
+              </Avatar>
+              <h1 className="mt-4 text-center text-lg font-bold text-gray-400 group-hover:text-black dark:text-gray-500 group-hover:dark:text-white">
                 Codefug
-              </div>
-            </Link>
-          </SidebarGroupLabel>
-          <SidebarContent className="group min-h-full px-2">
-            <SidebarGroup>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  {NAVIGATION_ITEMS.map((item) => (
-                    <SidebarMenuItem key={item.label}>
-                      <SidebarMenuButton asChild>
-                        <Link
-                          href={item.href}
-                          passHref
-                          className={cn(
-                            `rounded-md px-2 text-gray-400 transition duration-500 ease-in-out hover:bg-accent hover:text-black hover:underline hover:drop-shadow-lg group-hover:text-black hover:dark:text-white group-hover:dark:text-white`,
-                          )}
-                          onClick={toggleSidebar}
-                        >
-                          {item.label}
-                        </Link>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))}
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </SidebarGroup>
-          </SidebarContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter />
+              </h1>
+              <p className="text-sm text-gray-400 group-hover:text-black dark:text-gray-500 group-hover:dark:text-white">
+                Web Frontend Developer
+              </p>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {NAVIGATION_ITEMS.map((item) => (
+                  <SidebarMenuItem key={item.label}>
+                    <SidebarMenuButton asChild>
+                      <Link
+                        href={item.href}
+                        passHref
+                        className={cn(
+                          `rounded-md px-2 text-gray-400 transition duration-500 ease-in-out hover:bg-accent hover:text-black hover:underline hover:drop-shadow-lg group-hover:text-black hover:dark:text-white group-hover:dark:text-white`,
+                        )}
+                        onClick={toggleSidebar}
+                      >
+                        {item.label}
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter />
+      </ScrollArea>
     </Sidebar>
   );
 }
