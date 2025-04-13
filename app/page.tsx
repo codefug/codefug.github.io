51d34fd065ad1614606920e50ff6d1efd2e0b439
codefug <86544979+codefug@users.noreply.github.com>
2025-04-13 22:02:01 +0900

    feat: banner 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import PostGallery from "@/components/postGallery";
+import Banner from "@/components/ui/banner";
 import BlockHeader from "@/components/ui/block-header";
 import { getFrontMatterList } from "@/lib/posts";
 
@@ -6,8 +7,11 @@ export default function Home() {
   const frontMatterList = getFrontMatterList();
 
   return (
-    <BlockHeader title="최신 포스트">
-      <PostGallery postInfoList={frontMatterList} />
-    </BlockHeader>
+    <div>
+      <Banner />
+      <BlockHeader title="최신 포스트">
+        <PostGallery postInfoList={frontMatterList} />
+      </BlockHeader>
+    </div>
   );
 }
