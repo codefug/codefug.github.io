8abb89a455c72456bc2042979174f120a3760fa8
codefug <robot9917@gmail.com>
2025-04-19 15:35:23 +0900

    feat: postCategoryList 홈 라우트에서 재사용 정리

---
+++ components/postSwiper/postCategorySwiper.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import {
+  AppCarouselNext,
+  AppCarouselPrevious,
+} from "../ui/app-carousel-button";
+import { Badge } from "../ui/badge";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+
+export default function PostCategorySwiper({
+  categoryList,
+  setValue,
+  value,
+}: {
+  value?: string;
+  setValue?: (value: string) => void;
+  categoryList: { category: string; id: string }[];
+}) {
+  return (
+    <Carousel className="mb-4 px-10">
+      <CarouselContent>
+        {categoryList.map(({ category, id }) => (
+          <CarouselItem key={id + category} className="basis-auto">
+            <button onClick={() => setValue && setValue(category)}>
+              <Badge
+                className="h-8 text-sm"
+                variant={category === value ? "default" : "outline"}
+              >
+                {category}
+              </Badge>
+            </button>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <AppCarouselPrevious />
+      <AppCarouselNext />
+    </Carousel>
+  );
+}
