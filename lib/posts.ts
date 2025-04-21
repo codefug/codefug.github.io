c3b246ef8ee8249557ee1314f8171224f295ef38
codefug <robot9917@gmail.com>
2025-04-22 08:56:23 +0900

    feat: 포스트 최신순으로 정렬 (기본값으로)

--- lib/posts.ts
+++ lib/posts.ts
@@ -6,15 +6,17 @@ import grayMatter from "gray-matter";
 const postsDirectory = join(process.cwd(), "markdown");
 const folderNames = readdirSync(postsDirectory);
 const getFrontMatterList = () =>
-  folderNames.map((folderName) => {
-    const fullPath = join(postsDirectory, folderName, "frontmatter.mdx");
-    const fileContents = readFileSync(fullPath, "utf8");
-    const matterResult = grayMatter(fileContents);
+  folderNames
+    .map((folderName) => {
+      const fullPath = join(postsDirectory, folderName, "frontmatter.mdx");
+      const fileContents = readFileSync(fullPath, "utf8");
+      const matterResult = grayMatter(fileContents);
 
-    return {
-      id: folderName,
-      ...(matterResult.data as Omit<FrontMatter, "id">),
-    };
-  });
+      return {
+        id: folderName,
+        ...(matterResult.data as Omit<FrontMatter, "id">),
+      };
+    })
+    .reverse(); // 최신순으로 정렬
 
 export default getFrontMatterList;
