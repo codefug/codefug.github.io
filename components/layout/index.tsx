40921c50b3a45a87db3d95fa105471ef89c42bdc
codefug <robot9917@gmail.com>
2025-04-24 09:01:41 +0900

    feat: scrollToTopButton 구현

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -6,6 +6,7 @@ import { AppProgressProvider } from "@bprogress/next";
 import Footer from "../ui/footer";
 import Header from "../ui/header/header";
 import { useSidebar } from "../ui/sidebar";
+import { ScrollUpButton } from "../ui/scroll-up-button";
 
 export default function Layout({ children }: { children: ReactNode }) {
   const { isMobile, state } = useSidebar();
@@ -30,6 +31,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         <div className="mx-auto w-full max-w-[1400px] flex-1 px-4">
           {children}
         </div>
+        <ScrollUpButton />
         <Footer />
       </main>
     </AppProgressProvider>
