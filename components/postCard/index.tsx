b385cbfdd7898e8bfe111bd2c4b3a909b761cde7
codefug <86544979+codefug@users.noreply.github.com>
2025-04-27 00:10:05 +0900

    fix: 이미지 경고 해결

--- components/postCard/index.tsx
+++ components/postCard/index.tsx
@@ -3,8 +3,8 @@ import { POST_PATH } from "@/constants/path";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { useMemo } from "react";
-import { Card } from "../ui/card";
 import { Badge } from "../ui/badge";
+import { Card } from "../ui/card";
 
 export default function PostCard({
   categories,
@@ -20,19 +20,20 @@ export default function PostCard({
     // SSG로 렌더링된 포스트 링크는 CSR로 이동시킬 수 없다. ( HTML을 받아야 한다. )
     <a href={linkHref} aria-label={`${title} 포스트 읽기`} rel="bookmark">
       <Card className="group cursor-pointer overflow-hidden bg-white transition-all duration-300 hover:bg-gray-50 hover:shadow-xl dark:bg-gray-800 dark:hover:bg-gray-700">
-        <Image
-          priority={false}
-          src={header.teaser}
-          alt={title}
-          height={208}
-          width={208}
-          sizes="(max-width: 768px) 100vw, 50vw"
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
-          className={cn(
-            "!h-52 w-full object-cover object-center transition-transform duration-300 group-hover:scale-105",
-          )}
-        />
+        <div className="relative h-52 w-full overflow-hidden rounded-t-md">
+          <Image
+            priority={false}
+            src={header.teaser}
+            alt={title}
+            fill
+            sizes="(max-width: 768px) 100vw, 50vw"
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+            className={cn(
+              "h-52 w-full object-cover object-center transition-transform duration-300 group-hover:scale-105",
+            )}
+          />
+        </div>
         <div className="p-5">
           <div className="mb-3 flex items-center justify-between">
             <div>
