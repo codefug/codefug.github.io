78f53d9542f1d69a1c771139a53ec842aa1b9cce
codefug <86544979+codefug@users.noreply.github.com>
2025-05-06 02:47:49 +0900

    feat: about 페이지(portfolio용) 초기 구현

---
+++ constants/about/index.ts
@@ -0,0 +1,320 @@
+export const SKILLS = {
+  strong: [
+    {
+      subtitle: "Framework / Library",
+      list: [
+        {
+          alt: "React",
+          src: "https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB",
+        },
+        {
+          alt: "Next.js",
+          src: "https://img.shields.io/badge/next.js-%23000000.svg?style=for-the-badge&logo=nextdotjs&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "Language",
+      list: [
+        {
+          alt: "TypeScript",
+          src: "https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white",
+        },
+        {
+          alt: "JavaScript",
+          src: "https://img.shields.io/badge/javascript-%23F7DF1E.svg?style=for-the-badge&logo=javascript&logoColor=black",
+        },
+      ],
+    },
+    {
+      subtitle: "State Management",
+      list: [
+        {
+          alt: "Zustand",
+          src: "https://img.shields.io/badge/zustand-%23000000.svg?style=for-the-badge&logo=react&logoColor=white",
+        },
+        {
+          alt: "Tanstack Query",
+          src: "https://img.shields.io/badge/-TanStack%20Query-%23FF4154?style=for-the-badge&logo=react-query&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "Testing",
+      list: [
+        {
+          alt: "MSW",
+          src: "https://img.shields.io/badge/mock%20service%20worker-%23E33332.svg?style=for-the-badge&logo=msw&logoColor=white",
+        },
+        {
+          alt: "React Testing Library",
+          src: "https://img.shields.io/badge/-Testing%20Library-%23E33332?style=for-the-badge&logo=testing-library&logoColor=white",
+        },
+        {
+          alt: "Jest",
+          src: "https://img.shields.io/badge/jest-%23C21325.svg?style=for-the-badge&logo=jest&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "Animation",
+      list: [
+        {
+          alt: "Framer Motion",
+          src: "https://img.shields.io/badge/framer--motion-%23000000.svg?style=for-the-badge&logo=framer&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "CSS",
+      list: [
+        {
+          alt: "Tailwind CSS",
+          src: "https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "CI / CD",
+      list: [
+        {
+          alt: "Vercel",
+          src: "https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white",
+        },
+        {
+          alt: "Netlify",
+          src: "https://img.shields.io/badge/netlify-%23000000.svg?style=for-the-badge&logo=netlify&logoColor=white",
+        },
+        {
+          alt: "GitHub Actions",
+          src: "https://img.shields.io/badge/githubactions-%232671E5.svg?style=for-the-badge&logo=githubactions&logoColor=white",
+        },
+        {
+          alt: "Docker",
+          src: "https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "Others",
+      list: [
+        {
+          alt: "React Router",
+          src: "https://img.shields.io/badge/react--router-%23CA4245.svg?style=for-the-badge&logo=react-router&logoColor=white",
+        },
+        {
+          alt: "React Hook Form",
+          src: "https://img.shields.io/badge/React%20Hook%20Form-%23EC5990.svg?style=for-the-badge&logo=reacthookform&logoColor=white",
+        },
+        {
+          alt: "Git",
+          src: "https://img.shields.io/badge/git-%23F05032.svg?style=for-the-badge&logo=git&logoColor=white",
+        },
+        {
+          alt: "GitHub",
+          src: "https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white",
+        },
+        {
+          alt: "Zod",
+          src: "https://img.shields.io/badge/zod-%232D3748.svg?style=for-the-badge&logo=zod&logoColor=white",
+        },
+      ],
+    },
+  ],
+  knowledgeable: [
+    {
+      subtitle: "CSS",
+      list: [
+        {
+          alt: "Styled Components",
+          src: "https://img.shields.io/badge/styled--components-%23db7093.svg?style=for-the-badge&logo=styled-components&logoColor=white",
+        },
+        {
+          alt: "Sass",
+          src: "https://img.shields.io/badge/sass-%23CC6699.svg?style=for-the-badge&logo=sass&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "Component Documentation",
+      list: [
+        {
+          alt: "Storybook",
+          src: "https://img.shields.io/badge/storybook-%23FF4785.svg?style=for-the-badge&logo=storybook&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "Backend",
+      list: [
+        {
+          alt: "Express",
+          src: "https://img.shields.io/badge/express-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB",
+        },
+        {
+          alt: "MongoDB",
+          src: "https://img.shields.io/badge/mongodb-%2347A248.svg?style=for-the-badge&logo=mongodb&logoColor=white",
+        },
+      ],
+    },
+    {
+      subtitle: "GTM / GA",
+      list: [
+        {
+          alt: "Google Tag Manager",
+          src: "https://img.shields.io/badge/google%20tag%20manager-4285F4?style=for-the-badge&logo=google-tag-manager&logoColor=white",
+        },
+        {
+          alt: "Google Analytics",
+          src: "https://img.shields.io/badge/google%20analytics-E37400?style=for-the-badge&logo=google-analytics&logoColor=white",
+        },
+      ],
+    },
+  ],
+};
+
+export const PROJECTS = [
+  {
+    title: "B2B AI기반 보고서 분석 서비스 개발",
+    duration: "2025.01 ~",
+    role: "Web Frontend Developer (FE 2명, BE 2명 중 FE로 참여)",
+    description: "삼일 PWC AC 에서 진행한 AI 기반 보고서 분석 서비스",
+    stack: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "Zustand",
+      "Tanstack Query",
+      "GitHub Actions",
+      "Docker",
+      "Docker Compose",
+      "Jest",
+      "React Testing Library",
+      "Azure VM",
+      "Nginx",
+    ],
+    features: [
+      "고급 PDF Viewer 기능 구현 및 사용자 경험 최적화",
+      "Docker 이미지 및 번들 사이즈 최적화",
+      "사설 인증서 문제 해결 및 SSR 통신 환경 최적화",
+    ],
+  },
+  {
+    title: "B2B 서비스 통합 관리 포털",
+    duration: "2024.01 ~",
+    role: "Frontend Developer",
+    description:
+      "삼일 PWC AC 에서 진행한 사내 서비스를 통합 관리할 수 있는 포털 서비스",
+    stack: [
+      "React(Vite)",
+      "TypeScript",
+      "Tailwind CSS",
+      "Zustand",
+      "React Router",
+      "Axios",
+      "Github Actions",
+      "Docker",
+    ],
+    features: [
+      "사용자 경험 향상을 위한 부드러운 화면 전환 구현",
+      "리렌더링 성능 개선을 위한 구조적인 상태 관리 개편",
+      "Vite로의 빌드 시스템 전환 및 번들 사이즈 최적화",
+      "보안 표준 준수를 위한 XSS 및 URL 인젝션 방지 구현",
+    ],
+  },
+  {
+    title: "순록의 편지",
+    duration: "2024.12.02 ~ 2025.01.20",
+    role: "Frontend Developer",
+    description:
+      "본인과 타인에게 음성 및 글 형태로 예약 편지를 작성+전달할 수 있는 서비스",
+    stack: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "Zustand",
+      "Axios",
+      "Github Actions",
+      "MSW",
+      "GTM/GA",
+      "pnpm",
+    ],
+    features: [
+      "AbortController를 활용한 요청 관리 체계 개선",
+      "SNS 중심 사용성을 고려한 인앱 브라우저 한계 해결을 위한 브라우저 전환 기능 구현",
+      "List Virtualization을 통한 대용량 목록 렌더링 최적화",
+    ],
+  },
+  {
+    title: "꼼꼼",
+    duration: "2024.07.26 ~ 2024.09.20",
+    role: "Frontend Developer",
+    description: "팀을 구성하고 일정을 관리하여 꼼꼼이가 되는 서비스",
+    stack: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "Zustand",
+      "Tanstack Query",
+      "Github Actions",
+      "Storybook",
+      "GTM/GA",
+      "pnpm",
+    ],
+    features: [
+      "zustand 기반 useOverlay 커스텀 훅을 통한 오버레이 시스템 개발",
+      "서버 액션을 활용하여 refresh token rotation을 위한 fetch 함수 구현",
+      "streaming 방식과 낙관적 업데이트, 무한 스크롤이 결합된 쿼리 관리",
+      "startTransition에 대한 이해, progress bar 구현에 대한 고민",
+    ],
+  },
+  {
+    title: "급페이",
+    duration: "2024.06.20 ~ 2024.07.07",
+    role: "Frontend Developer",
+    description:
+      "급하게 일손이 필요한 자리에 더 많은 시급을 제공해서 아르바이트생을 구할 수 있는 서비스",
+    stack: [
+      "Next.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "Axios",
+      "Github Actions",
+      "MSW",
+      "yarn",
+    ],
+    features: [
+      "전역 상태를 걷어내고 서버 쿠키를 활용하여 최근 본 공고 구현",
+      "알림창 무한 스크롤 mock fetching를 위한 MSW 연동",
+      "vercel CLI와 github action을 이용한 vercel organization 무료 자동 배포",
+      "미들웨어와 쿠키를 활용한 계정 분기 처리",
+    ],
+  },
+  {
+    title: "Fandom-k",
+    duration: "2024.04.30 ~ 2024.05.17",
+    role: "Frontend Developer",
+    description: "좋아하는 아이돌을 투표하고 후원할 수 있는 서비스",
+    stack: [
+      "React",
+      "TypeScript",
+      "Tanstack Query",
+      "Recoil",
+      "Tailwind CSS",
+      "Vercel",
+      "GitHub Actions",
+    ],
+    features: [
+      "Interception observer API를 활용하여 무한 스크롤 구현",
+      "router 이동시 스크롤을 상단 고정하는 공통 컴포넌트 구현",
+      "Github Action을 이용한 Netlify 자동 배포",
+    ],
+  },
+];
+
+export const GROWTH_JOURNEY = [
+  "제가 학습이라는 것에 의지를 가지게 된 건 고등학교 2학년 때부터였습니다. 만년 5등급이던 저는 진짜 하고 싶은 일이 무엇인지 찾고 싶어졌고 그것을 위한 수단으로 학습이 필요하다고 느끼게 되었습니다. 그 생각이 든 순간부터는 학습에 전념하면서 수학과 영어에 흥미를 갖게 되었습니다. 결과적으로 1년 만에 두 과목 전부 1등급을 맞게 되었습니다. 이때 꾸준히 한다는 것의 중요성과 남들이 하기 싫어하는 것이어도 내가 좋아할 수 있다는 생각을 갖게 되었습니다. 그리고 내가 좋아하는 일을 찾아야겠다는 생각을 갖고 대학교로 오게 됩니다.",
+  "대학교는 저에게 하고 싶은 일을 찾는 과정이었다고 생각합니다. 남들 앞에 두려움 없이 서는 저를 만들기 위해 발표를 할 수 있는 활동들은 전부 임하였고 동아리도 밴드부 같은 외향적인 동아리에 들어갔었습니다. 수많은 발표 속에서 알게 된 건 저는 제가 알고 있는 지식을 공유하는 것을 즐긴다는 것입니다. 이는 레크리에이션 강사처럼 사람들을 즐겁게 만들기 위해 사람들 앞에 나서는 직업은 저와는 맞지 않다는 것이었죠. 그로 인해서 하고 싶은 일을 찾기 위한 휴학을 결정하게 됩니다.",
+  "휴학을 결정하게 된 이후 들어간 곳은 VR 테마파크였습니다. 사람들도 만나면서 돈도 벌 수 있다고 생각했습니다. 생각하지 못했었지만 개발이라는 분야에 관심을 갖게 됩니다. VR 놀이기구는 VR로 화면이 나오면서 화면에 맞춰 기구가 움직이며 동작했습니다. 여기서 화면을 띄우는 프로그램은 타사의 프로그램을 가져온 경우가 많았는데 타사 프로그램을 금전적인 이유로 자사 프로그램으로 교체하는 일이 발생했었습니다. 금전적인 이유로 교체가 발생했지만 선택은 더 큰 금전적인 손해를 입히게 됩니다. 기존에 인기 있던 기구들은 연령대가 낮춰진 자사 프로그램으로 인해서 인기가 사라진 것입니다. 고객이 점점 없어지던 테마파크는 코로나와 함께 사업을 끝내게 됩니다. 안타까운 일이지만 사용자에 맞닿는 부분의 중요성을 알게 된 계기였습니다. 이와 관련된 개발에 대해서 찾아보다가 프론트엔드라는 직무를 알게 되었습니다.",
+  "이후 부트캠프에서 프로젝트를 진행하면서 기술에 대해 깊게 공부하게 되었습니다. 기술을 공부하면서는 실생활에서 기술적인 요소들이 겹쳐서 보이기 위해 시작합니다. CSS를 공부할 때는 엘리베이터 버튼을 웹페이지로 어떻게 구현하면 될까를 고민하는 거나 스트리밍 사이트에서 컴포넌트를 어떻게 구성하면 채팅과 영상을 리렌더링 없이 잘 분리할 수 있는가 같은 기술적인 호기심이 마구 생기면서 개발이라는 분야에 푹 빠지게 되었던 것 같습니다. 그러다가 점점 실제 프로덕트를 만지면서 고객들과 소통하며 개발하고 싶게 되었고 더욱 더 노력하고 싶어졌습니다. 많은 일들을 거쳐서 온 만큼 조금 더 열심히 열정적으로 프론트엔드 개발자로서의 길을 걷고 있습니다.",
+];
