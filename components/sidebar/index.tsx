2b64281cc6b8382389dd936d42a197678556702f
codefug <robot9917@gmail.com>
2025-06-03 20:59:15 +0900

    chore: scroll-area 삭제

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -21,7 +21,6 @@ import { AnchorHTMLAttributes, useMemo, useRef } from "react";
 import { FrontMatter } from "@/constants/mdx";
 import { ChevronDown } from "lucide-react";
 import { POST_PATH } from "@/constants/path";
-import { ScrollArea } from "../ui/scroll-area";
 import {
   Collapsible,
   CollapsibleContent,
@@ -55,69 +54,70 @@ export default function AppSidebar({
 
   return (
     <Sidebar ref={ref}>
-      <ScrollArea className="h-full w-full overflow-y-auto">
-        <SidebarContent className="group relative gap-0">
-          <div className="absolute h-[150px] w-full bg-primary" />
-          <SidebarGroup>
-            <SidebarGroupLabel className="mb-2 mt-4 flex justify-center px-4 text-sm font-bold text-gray-400 group-hover:text-white">
-              Codefug
-            </SidebarGroupLabel>
-            <SidebarGroupContent className="flex min-h-full flex-col items-center px-2">
-              <Avatar className="h-40 w-40">
-                <AvatarImage src="/images/me.jpg" />
-                <AvatarFallback className="group-hover:text-black group-hover:dark:text-white">
-                  이승현
-                </AvatarFallback>
-              </Avatar>
-              <h1 className="mt-4 text-center text-lg font-bold text-gray-400 group-hover:text-black dark:text-gray-500 group-hover:dark:text-white">
+      <SidebarContent
+        className="group relative gap-0"
+        style={{ scrollbarWidth: "none" }}
+      >
+        <div className="absolute h-[150px] w-full bg-primary" />
+        <SidebarGroup>
+          <SidebarGroupLabel className="mb-2 mt-4 flex justify-center px-4 text-sm font-bold text-gray-400 group-hover:text-white">
+            Codefug
+          </SidebarGroupLabel>
+          <SidebarGroupContent className="flex min-h-full flex-col items-center px-2">
+            <Avatar className="h-40 w-40">
+              <AvatarImage src="/images/me.jpg" />
+              <AvatarFallback className="group-hover:text-black group-hover:dark:text-white">
                 이승현
-              </h1>
-              <p className="text-sm text-gray-400 group-hover:text-black dark:text-gray-500 group-hover:dark:text-white">
-                Web Frontend Developer
-              </p>
-            </SidebarGroupContent>
-          </SidebarGroup>
-          {Object.keys(frontMatterListArrangedByCategories).map((category) => (
-            <CollapsiblePostList
-              key={category.toString()}
-              category={category}
-              frontMatterList={frontMatterListArrangedByCategories[category]}
-            />
-          ))}
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {NAVIGATION_ITEMS.map((item) => (
-                  <SidebarMenuItem key={item.label}>
-                    <SidebarMenuButton asChild>
-                      {item.label === "Portfolio" ? (
-                        <SidebarAnchorButton
-                          navType="a"
-                          href="https://www.figma.com/proto/KSWARGDkXi9Wt8ARq2uGCa/leeseounghyun-resume?node-id=401-2&node-type=canvas&t=z2H9bL74afXrrgPS-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1"
-                          target="_blank"
-                          key={item.label}
-                        >
-                          {item.label}
-                        </SidebarAnchorButton>
-                      ) : (
-                        <SidebarAnchorButton
-                          navType="link"
-                          href={item.href}
-                          passHref
-                          onClick={toggleSidebar}
-                        >
-                          {item.label}
-                        </SidebarAnchorButton>
-                      )}
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-        <SidebarFooter />
-      </ScrollArea>
+              </AvatarFallback>
+            </Avatar>
+            <h1 className="mt-4 text-center text-lg font-bold text-gray-400 group-hover:text-black dark:text-gray-500 group-hover:dark:text-white">
+              이승현
+            </h1>
+            <p className="text-sm text-gray-400 group-hover:text-black dark:text-gray-500 group-hover:dark:text-white">
+              Web Frontend Developer
+            </p>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        {Object.keys(frontMatterListArrangedByCategories).map((category) => (
+          <CollapsiblePostList
+            key={category.toString()}
+            category={category}
+            frontMatterList={frontMatterListArrangedByCategories[category]}
+          />
+        ))}
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {NAVIGATION_ITEMS.map((item) => (
+                <SidebarMenuItem key={item.label}>
+                  <SidebarMenuButton asChild>
+                    {item.label === "Portfolio" ? (
+                      <SidebarAnchorButton
+                        navType="a"
+                        href="https://www.figma.com/proto/KSWARGDkXi9Wt8ARq2uGCa/leeseounghyun-resume?node-id=401-2&node-type=canvas&t=z2H9bL74afXrrgPS-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1"
+                        target="_blank"
+                        key={item.label}
+                      >
+                        {item.label}
+                      </SidebarAnchorButton>
+                    ) : (
+                      <SidebarAnchorButton
+                        navType="link"
+                        href={item.href}
+                        passHref
+                        onClick={toggleSidebar}
+                      >
+                        {item.label}
+                      </SidebarAnchorButton>
+                    )}
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
     </Sidebar>
   );
 }
