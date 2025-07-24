f6607547d527f385136b55f1a58b851e599a2c82
codefug <86544979+codefug@users.noreply.github.com>
2025-07-25 01:19:18 +0900

    feat: about 라우트 헤딩 컴포넌트 수정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,4 @@
-import AboutHeading from "@/components/about/banner";
+import AboutHeading from "@/components/about/about-heading";
 import Contact from "@/components/about/contact";
 import Description from "@/components/about/description";
 import Education from "@/components/about/education";
@@ -6,14 +6,12 @@ import GrowthJourney from "@/components/about/growth-journey";
 import Projects from "@/components/about/projects";
 import Skills from "@/components/about/skills";
 import StudyGroup from "@/components/about/study-group";
-import Title from "@/components/about/title";
 import WorkExperience from "@/components/about/work-experience";
 
 export default function Page() {
   return (
     <>
       <AboutHeading />
-      <Title />
       <Description />
       <WorkExperience />
       <Projects />
