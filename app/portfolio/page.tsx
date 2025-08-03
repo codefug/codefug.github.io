98ddb862f303b6d8d1b62500f686a6bbd216d0ba
codefug <86544979+codefug@users.noreply.github.com>
2025-08-03 14:10:42 +0900

    feat<portfolio>: growth-journey 추가

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,13 +1,15 @@
+import GrowthJourney from "@/components/growth-journey";
 import AboutHeading from "@/components/portfolio/about-heading";
 import { FadeInSection } from "@/components/portfolio/fadeInSection";
 import { FloatingShapesBackground } from "@/components/portfolio/floating-shapes-background";
-import Skills from "@/components/portfolio/skills";
 import WorkExperience from "@/components/portfolio/work-experience";
 import dynamic from "next/dynamic";
 
 const Projects = dynamic(() => import("@/components/portfolio/projects"));
 
 const Education = dynamic(() => import("@/components/portfolio/education"));
+
+const Skills = dynamic(() => import("@/components/portfolio/skills"));
 
 const StudyGroup = dynamic(() => import("@/components/portfolio/study-group"));
 
@@ -19,15 +21,18 @@ export default function Page() {
       <FloatingShapesBackground />
       <div className="relative z-10">
         <AboutHeading />
+        <GrowthJourney />
         <WorkExperience />
-        <Skills />
+        <FadeInSection delay={0} className="mb-8">
+          <Skills />
+        </FadeInSection>
         <FadeInSection delay={0} className="mb-8">
           <Projects />
         </FadeInSection>
-        <FadeInSection delay={0} className="mb-8">
+        <FadeInSection delay={0} className="mb-0">
           <Education />
         </FadeInSection>
-        <FadeInSection delay={0} className="mb-8">
+        <FadeInSection delay={0} className="mb-2">
           <StudyGroup />
         </FadeInSection>
         <FadeInSection delay={0} className="mb-8">
