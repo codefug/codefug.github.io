ba2453911a5046a5b34842bfe6fa9240f6d64e1e
codefug <robot9917@gmail.com>
2025-08-19 21:02:35 +0900

    feat<portfolio>: questionBlock 컴포넌트 구현 및 question 연결

--- components/growth-journey/index.tsx
+++ components/growth-journey/index.tsx
@@ -1,9 +1,14 @@
 import { GROWTH_JOURNEY } from "@/constants/about";
+import { ComponentPropsWithoutRef } from "react";
 import { HeadComponent } from "../portfolio/HeadComponent";
 
-export default function GrowthJourney() {
+interface Props {
+  className?: ComponentPropsWithoutRef<"section">["className"];
+}
+
+export default function GrowthJourney({ className }: Props) {
   return (
-    <section>
+    <section className={className}>
       <HeadComponent>Growth Journey</HeadComponent>
       <div className="flex flex-col gap-2 whitespace-pre-line">
         {GROWTH_JOURNEY}
