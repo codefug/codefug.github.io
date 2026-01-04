830bc6431793bc216d96ef3e2406113fe97d90d6
codefug <robot9917@gmail.com>
2026-01-05 01:59:54 +0900

    feat: Zustand로 포스트 마운트 상태 관리 추가

--- components/post/PostContent.tsx
+++ components/post/PostContent.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { useLocale } from "next-intl";
-import { useLayoutEffect, useState } from "react";
+import { useLayoutEffect } from "react";
 import type { Locale } from "@/i18n/config";
 import { mdxMap } from "@/lib/mdxMap";
+import { usePostContentStore } from "@/store/use-post-content-store";
 import { PostContentSkeleton } from "./PostContentSkeleton";
 
 type Props = {
@@ -12,11 +13,16 @@ type Props = {
 
 export function PostContent({ postId }: Props) {
   const locale = useLocale() as Locale;
-  const [isMounted, setIsMounted] = useState(false);
+  const { isMounted, setIsMounted } = usePostContentStore();
 
   useLayoutEffect(() => {
     setIsMounted(true);
-  }, []);
+
+    // 컴포넌트 언마운트 시 상태 초기화
+    return () => {
+      setIsMounted(false);
+    };
+  }, [setIsMounted]);
 
   // locale이 로드되기 전까지 스켈레톤 표시
   if (!isMounted) {
