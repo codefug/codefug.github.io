38b7b6bd19e07705364a2dbb86ee764b22c1de72
codefug <robot9917@gmail.com>
2026-05-19 20:19:44 +0900

    feat: RSS 피드 추가 및 푸터에 RSS 링크 추가

---
+++ app/rss.xml/route.ts
@@ -0,0 +1,40 @@
+import { getFrontMatterListForAllLocales } from "@/lib/posts";
+
+const BASE_URL = "https://codefug.github.io";
+
+export const dynamic = "force-static";
+
+export async function GET() {
+  const posts = getFrontMatterListForAllLocales().ko;
+
+  const items = posts
+    .map(
+      (post) => `
+    <item>
+      <title><![CDATA[${post.title}]]></title>
+      <link>${BASE_URL}/posts/${post.id}/</link>
+      <guid>${BASE_URL}/posts/${post.id}/</guid>
+      <description><![CDATA[${post.excerpt}]]></description>
+      <pubDate>${new Date(post.date).toUTCString()}</pubDate>
+    </item>`,
+    )
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>CodeFug Blog</title>
+    <link>${BASE_URL}</link>
+    <description>개발자 코드퍼그의 기술 블로그</description>
+    <language>ko</language>
+    <atom:link href="${BASE_URL}/rss.xml" rel="self" type="application/rss+xml"/>
+    ${items}
+  </channel>
+</rss>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
+}
