dc0e9e6a4897687fd2779152791ccb9e066a8a29
codefug <robot9917@gmail.com>
2026-05-21 21:51:30 +0900

    fix: scrollbar-gutter 제거, -mx-4 방식으로 풀 블리드 복구
    scrollbar-gutter는 구버전 Safari 미지원으로 크로스브라우저 보장 불가.
    부모 px-4를 -mx-4로 상쇄하는 방식으로 교체.

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default async function Home() {
         )}
       />
       {showBirthdayBanner && <BirthdayBanner />}
-      <section className="my-10 lg:mt-0">
+      <section className="-mx-4 my-10 lg:mt-0">
         <HeroSection
           postCount={frontMatterListByLocale[defaultLocale].length}
         />
